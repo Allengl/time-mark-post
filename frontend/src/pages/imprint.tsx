543492af09d29d83358d2bdc8a8b2c95bfe34921
root <root@Allengl>
2024-03-03 13:49:51 +0800

    feat: Navbar, Next_Link, Next_Image, Footer

---
+++ frontend/src/pages/imprint.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ImprintPage = () => {
+  return (
+    <div>
+      <h1>Imprint</h1>
+      <p>Put some contact information here.</p>
+    </div>
+  );
+};
+
+export default ImprintPage;
